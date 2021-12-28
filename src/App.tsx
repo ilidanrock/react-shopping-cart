2e2e1d9d7a1c9c99f131be6e184293adfd4ef084
Luis Rivera <rluis747@gmail.com>
2021-12-28 18:13:10 -0500

    Setting up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState }  from 'react';
+import { useQuery } from "react-query";
+//Components 
+import Drawer from '@material-ui/core/Drawer'
+import { LinearProgress } from '@material-ui/core';
+import  Grid  from '@material-ui/core/Grid';
+import AddShoopingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from '@material-ui/core/Badge'
+//Styles
+import { Wrapper } from "./App.styles";
 
-function App() {
+export type CartItemType = {
+  
+}
+
+const getProducts = async () => 
+  await (await fetch('https://fakestoreapi.com')).json()
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Start
     </div>
   );
 }
