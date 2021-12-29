b9b3bf52103dd4ff5624e94405e96d25748bc673
Luis Rivera <rluis747@gmail.com>
2021-12-29 16:16:00 -0500

    I finished grid for cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState }  from 'react';
 import { useQuery } from "react-query";
 //Components 
+import { Item } from "./Item/Item";
 import Drawer from '@material-ui/core/Drawer'
 import { LinearProgress } from '@material-ui/core';
 import  Grid  from '@material-ui/core/Grid';
@@ -8,6 +9,7 @@ import AddShoopingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from '@material-ui/core/Badge'
 //Styles
 import { Wrapper } from "./App.styles";
+import { isQueryKey } from 'react-query/types/core/utils';
 
 export type CartItemType = {
   id:number;
@@ -19,15 +21,37 @@ export type CartItemType = {
   amount: number;
 }
 
-const getProducts = async (): Promise<CartItemType> => 
+const getProducts = async (): Promise<CartItemType[]> => 
   await (await fetch('https://fakestoreapi.com/products')).json()
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
+  const {data,isLoading,error} = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data);
+  
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem:CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if(isLoading) return <LinearProgress/>
+  
+  if(error) return <div>Something went wrong ...</div>
   return (
-    <div>
-      Start
-    </div>
+    <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}  >
+        Cart goes here
+      </Drawer>
+      <Grid container spacing={3} >
+        {data?.map(item =>(<Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={ ()=>handleAddToCart(item)} ></Item>
+          </Grid>)
+          )}
+      </Grid>
+    </Wrapper>
   );
-}
+};
 
 export default App;
