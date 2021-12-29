e7a711d5bff764e6ff5109207b41c8a10cd78ef3
Luis Rivera <rluis747@gmail.com>
2021-12-28 20:54:45 -0500

    Type CartItemType

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,17 @@ import Badge from '@material-ui/core/Badge'
 import { Wrapper } from "./App.styles";
 
 export type CartItemType = {
-  
+  id:number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title:string;
+  amount: number;
 }
 
-const getProducts = async () => 
-  await (await fetch('https://fakestoreapi.com')).json()
+const getProducts = async (): Promise<CartItemType> => 
+  await (await fetch('https://fakestoreapi.com/products')).json()
 
 const App = () => {
   return (
